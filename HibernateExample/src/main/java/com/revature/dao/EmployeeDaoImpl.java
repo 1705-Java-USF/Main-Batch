a67a7115a2cec04a5f6112408af9b0a62fc22b62
LucasC <lucas.costa.usa@hotmail.com>
2017-06-30 15:14:50 -0400

    hibernate example

---
+++ HibernateExample/src/main/java/com/revature/dao/EmployeeDaoImpl.java
@@ -0,0 +1,39 @@
+package com.revature.dao;
+
+import java.util.List;
+
+import com.revature.bean.Employee;
+
+public class EmployeeDaoImpl implements EmployeeDao{
+
+	@Override
+	public int insertEmployee(Employee emp) {
+
+		return 0;
+	}
+
+	@Override
+	public Employee getEmployeeById(Integer Id) {
+		
+		return null;
+	}
+
+	@Override
+	public List<Employee> getAllEmployees() {
+		
+		return null;
+	}
+
+	@Override
+	public void setEmployee(Employee emp) {
+		
+		
+	}
+
+	@Override
+	public void deleteEmployee(Integer id) {
+		
+		
+	}
+
+}
---
+++ HibernateExample/src/main/java/com/revature/util/HibernateUtil.java
@@ -0,0 +1,11 @@
+package com.revature.util;
+
+/*import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;*/
+
+public class HibernateUtil {
+
+	/*private static SessionFactory sessionFactory = new Configuration().
+														configure().
+														buildSessionFactory();*/
+}
